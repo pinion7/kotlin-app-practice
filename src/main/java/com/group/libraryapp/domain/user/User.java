da01b7d68c6a4b33f8f08e210f1c7f0e1a343098
박민수 <pinion7rl@gmail.com>
2022-10-01 22:16:30 +0900

    java -> kotlin 리팩토링: UserLoanHistory #2

--- src/main/java/com/group/libraryapp/domain/user/User.java
+++ src/main/java/com/group/libraryapp/domain/user/User.java
@@ -1,7 +1,6 @@
 package com.group.libraryapp.domain.user;
 
-import com.group.libraryapp.domain.Book;
-import com.group.libraryapp.domain.book.JavaBook;
+import com.group.libraryapp.domain.book.Book;
 import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -44,7 +43,7 @@ public class User {
   }
 
   public void loanBook(Book book) {
-    this.userLoanHistories.add(new UserLoanHistory(this, book.getName(), false));
+    this.userLoanHistories.add(new UserLoanHistory(this, book.getName(), false, null));
   }
 
   public void returnBook(String bookName) {
