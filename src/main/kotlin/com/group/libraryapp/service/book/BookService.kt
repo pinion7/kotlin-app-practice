c979adcb53b85057667337eb3e2a2f2cb439d769
박민수 <pinion7rl@gmail.com>
2022-10-10 11:02:45 +0900

    리팩토링 BookService #2

--- src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
+++ src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
@@ -1,4 +1,44 @@
 package com.group.libraryapp.service.book
 
-class BookService {
+import com.group.libraryapp.domain.book.Book
+import com.group.libraryapp.domain.book.BookRepository
+import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.domain.user.loanhistory.JavaUserLoanHistoryRepository
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistoryRepository
+import com.group.libraryapp.dto.book.request.BookLoanRequest
+import com.group.libraryapp.dto.book.request.BookRequest
+import com.group.libraryapp.dto.book.request.BookReturnRequest
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+open class BookService(
+    private val bookRepository: BookRepository,
+    private val userRepository: UserRepository,
+    private val userLoanHistoryRepository: UserLoanHistoryRepository
+) {
+
+    @Transactional
+    open fun saveBook(request: BookRequest) {
+        val book = Book(request.name)
+        bookRepository.save(book)
+    }
+
+    @Transactional
+    open fun loanBook(request: BookLoanRequest) {
+        val book = bookRepository.findByName(request.bookName).orElseThrow(::IllegalArgumentException)
+        if (userLoanHistoryRepository.findByBookNameAndIsReturn(request.bookName, false) != null) {
+            throw IllegalArgumentException("이미 대출되어 있는 책입니다.")
+        }
+
+        val user = userRepository.findByName(request.userName).orElseThrow(::IllegalArgumentException)
+        user.loanBook(book)
+    }
+
+    @Transactional
+    open fun returnBook(request: BookReturnRequest) {
+        val user = userRepository.findByName(request.userName).orElseThrow(::IllegalArgumentException)
+        user.returnBook(request.bookName)
+    }
 }
