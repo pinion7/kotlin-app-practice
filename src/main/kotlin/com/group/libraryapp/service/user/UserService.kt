4f677271d84e68d7d26b4512962bf11a9cf621a6
박민수 <pinion7rl@gmail.com>
2022-10-12 09:49:52 +0900

    리팩토링 BookSerivce #4

--- src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
+++ src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -5,6 +5,8 @@ import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.dto.user.request.UserCreateRequest
 import com.group.libraryapp.dto.user.request.UserUpdateRequest
 import com.group.libraryapp.dto.user.response.UserResponse
+import com.group.libraryapp.util.fail
+import com.group.libraryapp.util.findByIdOrThrow
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -28,13 +30,13 @@ open class UserService(
 
     @Transactional
     open fun updateUserName(request: UserUpdateRequest) {
-        val user = userRepository.findByIdOrNull(request.id) ?: throw IllegalArgumentException()
+        val user = userRepository.findByIdOrThrow(request.id)
         user.updateName(request.name)
     }
 
     @Transactional
     open fun deleteUser(name: String) {
-        val user = userRepository.findByName(name) ?: throw IllegalArgumentException()
+        val user = userRepository.findByName(name) ?: fail()
         userRepository.delete(user)
     }
 }
--- src/main/kotlin/com/group/libraryapp/util/ExceptionUtils.kt
+++ src/main/kotlin/com/group/libraryapp/util/ExceptionUtils.kt
@@ -1,4 +1,12 @@
 package com.group.libraryapp.util
 
-class ExceptionUtils {
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.findByIdOrNull
+
+fun fail(): Nothing {
+    throw IllegalArgumentException()
+}
+
+fun <T, ID> CrudRepository<T, ID>.findByIdOrThrow(id: ID): T {
+    return this.findByIdOrNull(id) ?: fail()
 }
