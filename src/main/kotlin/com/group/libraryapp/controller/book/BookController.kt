b3c98a65a2fb38f66aec04bd44e651a429c277d5
박민수 <pinion7rl@gmail.com>
2022-10-17 21:13:56 +0900

    Controller 리팩토링 #1

--- src/main/kotlin/com/group/libraryapp/controller/book/BookController.kt
+++ src/main/kotlin/com/group/libraryapp/controller/book/BookController.kt
@@ -3,11 +3,7 @@ package com.group.libraryapp.controller.book
 import com.group.libraryapp.dto.book.request.BookLoanRequest
 import com.group.libraryapp.dto.book.request.BookRequest
 import com.group.libraryapp.dto.book.request.BookReturnRequest
-import com.group.libraryapp.dto.book.request.JavaBookLoanRequest
-import com.group.libraryapp.dto.book.request.JavaBookRequest
-import com.group.libraryapp.dto.book.request.JavaBookReturnRequest
 import com.group.libraryapp.service.book.BookService
-import com.group.libraryapp.service.book.JavaBookService
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
