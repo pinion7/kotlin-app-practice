1dd4562c1602198341fd5eba8c8ae51b42294108
박민수 <pinion7rl@gmail.com>
2022-10-21 10:43:21 +0900

    fix

--- src/main/java/com/group/libraryapp/JavaLibraryAppApplication.java
+++ src/main/java/com/group/libraryapp/JavaLibraryAppApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaLibraryAppApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(JavaLibraryAppApplication.class, args);
   }
